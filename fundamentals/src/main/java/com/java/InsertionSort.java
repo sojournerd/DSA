0a879c9cd0727104abae1845cc280f345f9853da
Iman Swain <ihabdulr@buffalo.edu>
2024-11-14 00:34:28 -0600

    Added insertionSort, added verification tests

--- fundamentals/src/main/java/com/java/InsertionSort.java
+++ fundamentals/src/main/java/com/java/InsertionSort.java
@@ -2,27 +2,29 @@ package com.java;
 
 import java.util.Arrays;
 
-public class Bubblesort {
-    public static void main(String[] args) {
+public class InsertionSort {
+       public static void main(String[] args) {
        // empty
     }
 
     public static int[] sort(int[] array) {
-        bubbleSort(array);
+        insertionSort(array);
         return array;
     }
 
-    public static void bubbleSort (int[] array) {
+    public static void insertionSort (int[] array) {
         if (array.length == 1) return;
         
         for(int i = 0; i < array.length; i++){
             int j = i+1;
+            int lowest = i;
             while(j <= array.length-1){
-                if(array[i] > array[j]){
-                    swap(array, i, j);
+                if(array[j] < array[lowest]){
+                    lowest = j;
                 }
                 j++;
             }
+            swap(array, i, lowest);
         }
         return;
     }
